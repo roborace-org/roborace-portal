f4b402621e69a00e99aba838f07e9cafdce3be9b
csoftware-arigpt <ksoarigpt@gmail.com>
2023-11-16 13:35:33 +0000

    Contest table

--- roborace-ui/pages/Competitions.py
+++ roborace-ui/pages/Competitions.py
@@ -1,3 +1,25 @@
-import streamlt as st
+import streamlit as st
+import requests
+import pandas as pd
 
-st.write("Redirected")
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+table = requests.get("http://127.0.0.1:8000/api/competition")
+table = table.json()
+reformated_table = []
+try:
+    for i in range(0, len(table)):
+        reformated_table.append({"id": table[i]["id"], "Name": table[i]["name"], "Date": table[i]['date'], "Track length": table[i]['track_length']})
+    df = pd.DataFrame(reformated_table)
+    df.set_index('id', inplace=True)
+    st.table(df)
+except:
+    st.table()
