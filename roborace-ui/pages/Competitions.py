df267573d856697dacf0c130e2f411151d1a5c2e
csoftware-arigpt <ksoarigpt@gmail.com>
2023-11-22 12:23:56 +0000

    Bugfix

--- roborace-ui/pages/Competitions.py
+++ roborace-ui/pages/Competitions.py
@@ -45,45 +45,45 @@ if params=={}:
     table = requests.get("http://127.0.0.1:8000/api/competitions")
     table = table.json()
     reformated_table = []
-
-    for i in range(0, len(table)):
-        reformated_table.append({
+    try:
+        for i in range(0, len(table)):
+            reformated_table.append({
             "ID": table[i]["id"],
             "Name": table[i]["name"],
             "Date": table[i]['date'],
             "Track length": table[i]['track_length'],
             "Link": f"[Browse this competition](?id={table[i]['id']})"
-        })
-    df = pd.DataFrame(reformated_table)
-    df.set_index('ID', inplace=True)
+            })
+        df = pd.DataFrame(reformated_table)
+        df.set_index('ID', inplace=True)
 
-    md_table = df.to_markdown()
-    st.markdown(md_table)
+        md_table = df.to_markdown()
+        st.markdown(md_table)
+    except:
+        st.table()
 else:
-    request = requests.get(f"http://127.0.0.1:8000/api/competitions/{params['id'][0]}")
-    if cookie_manager.get(cookie="session-key") != None and cookie_validator() == cookie_manager.get(cookie="session-key"):
-        st.write("Manage competition")
-        if request.json() == {"error": "No info"}:
-            df = pd.DataFrame([{"Name": "", 'Is Allowed?': False}])
-        else: 
-            false = False
-            true = True
-            null = None
-            df = pd.DataFrame(eval(request.json()))
-        df_editable = st.data_editor(df,num_rows="dynamic", hide_index=False)
-        if st.button("Send new list"):
-            table_send = df_editable.to_json(orient="records")
-            print(table_send)
-            try:
-                data = {"authorization": str(cookie_manager.get(cookie="session-key")), "robots": table_send}
-                request = requests.post(f"http://127.0.0.1:8000/api/competitions/{params['id'][0]}", json=data)
-            except Exception as e:
-             st.error(f"Error. Info: {e}")
-    else:
-        if request.json() == {"error": "No info"}:
-            image = Image.open('notfound.jpg')
-            st.image(image, "No-info robot")
-        else:
-            newtable = eval(request.json())
-            df = pd.DataFrame(newtable)
-            st.table(df)
+            request = requests.get(f"http://127.0.0.1:8000/api/competitions/{params['id'][0]}")
+            if cookie_manager.get(cookie="session-key") != None and cookie_validator() == cookie_manager.get(cookie="session-key"):
+                st.write("Manage competition")
+                if request.json() == {"error": "No info"}:
+                    df = pd.DataFrame([{"Name": "", 'Is Allowed?': False}])
+                else: 
+                    false = False
+                    true = True
+                    null = None
+                    df = pd.DataFrame(eval(request.json()))
+                df_editable = st.data_editor(df,num_rows="dynamic", hide_index=False)
+                if st.button("Send new list"):
+                    table_send = df_editable.to_json(orient="records")
+                try:
+                    data = {"authorization": str(cookie_manager.get(cookie="session-key")), "robots": table_send}
+                    request = requests.post(f"http://127.0.0.1:8000/api/competitions/{params['id'][0]}", json=data)
+                except Exception as e:
+                    st.error(f"Error. Info: {e}")
+            if request.json() == {"error": "No info"}:
+                image = Image.open('notfound.jpg')
+                st.image(image, "No-info robot")
+            else:
+                newtable = eval(request.json())
+                df = pd.DataFrame(newtable)
+                st.table(df)
